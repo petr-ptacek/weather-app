7976c47fc726c958b8be1a458659215b83a1e45e
Petr Ptacek <petr.ptacek99@gmail.com>
2026-09-25 22:50:59 +0200

    feat(#repo): implement cities logic

---
+++ src/api/getCities.ts
@@ -0,0 +1,32 @@
+import type { City } from "../types/city.ts";
+
+export interface GetCitiesParams {
+  query: string;
+}
+
+const GEO_API_URL = "https://api.openweathermap.org/geo/1.0/direct";
+const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
+
+export async function getCities(params: GetCitiesParams): Promise<City[]> {
+
+  const url = new URL(GEO_API_URL);
+
+  Object.entries({
+    q: params.query,
+    limit: "5",
+    appid: API_KEY
+  }).forEach(([key, value]) => {
+    url.searchParams.append(key, String(value));
+  });
+
+
+  const response = await fetch(url);
+
+  if ( !response.ok ) {
+    throw new Error(
+      `Failed to fetch cities: ${ response.status } ${ response.statusText }`
+    );
+  }
+
+  return response.json();
+}
