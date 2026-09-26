f9f968d1e5b2cae981fdb7b9e6c8e385fd741cf7
Petr Ptacek <petr.ptacek99@gmail.com>
2026-09-26 18:51:56 +0200

    feat(#repo): improvements

--- src/controllers/HeaderControls.ts
+++ src/controllers/HeaderControls.ts
@@ -3,15 +3,28 @@ import type { MaybeHTMLElement } from "../types";
 export class HeaderControls {
   root: MaybeHTMLElement = null;
   locationName: MaybeHTMLElement = null;
+  locationIcon: MaybeHTMLElement = null;
 
   init() {
     this.root = document.getElementById("header-controls") ?? null;
     this.locationName = this.root?.querySelector(".location__name") ?? null;
+    this.locationIcon = this.root?.querySelector(".location__icon") ?? null;
   }
-
 
   setLocationName(name: string) {
     if ( !this.locationName ) return;
     this.locationName.innerText = name;
+
+    this.toggleLocationIcon(!name);
+  }
+
+  toggleLocationName(visible?: boolean) {
+    if ( !this.locationName ) return;
+    this.locationName.classList.toggle("hidden", visible);
+  }
+
+  toggleLocationIcon(visible?: boolean) {
+    if ( !this.locationIcon ) return;
+    this.locationIcon.classList.toggle("hidden", visible);
   }
 }
